b98688b9633a5f07e66803823c008434539a47b9
walidrifaii53@gmail.com <walidrifaii53@gmail.com>
2025-10-26 23:41:50 +0200

    fixe the env

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import { createNestApp } from './app.factory';
 
 async function bootstrap() {
   const app = await createNestApp();
-  await app.listen(3000);
-  console.log('🚀 Server running on http://localhost:3000');
-  console.log('📘 Swagger Docs available at http://localhost:3000/docs');
+
+  // ✅ Use dynamic port for Vercel
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port);
+  console.log(`🚀 Server running on http://localhost:${port}`);
+  console.log(`📘 Swagger Docs available at http://localhost:${port}/docs`);
 }
 bootstrap();
